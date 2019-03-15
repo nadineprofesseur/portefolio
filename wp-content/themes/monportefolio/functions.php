818586fac42f3e4b71ac8012dbb62df08bfc450a
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-15 15:16:45 -0400

    sidebar dynamique

---
+++ wp-content/themes/monportefolio/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function monportefolio_widgets_init() {
+	//require get_template_directory() . '/inc/widgets.php';
+	//register_widget( 'MonPortefolio_Sidebar' );
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Barre latérale', 'monportefolio' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Main sidebar that appears on the left.', 'monportefolio' ),
+			'before_widget' => '<div id="text-3" class="widget widget_text">',
+			'after_widget'  => '</div><hr class="divider-green">',
+			'before_title'  => '<div class="widget-title">',
+			'after_title'   => '</div>',
+		)
+	);
+
+}
+add_action( 'widgets_init', 'monportefolio_widgets_init' );
