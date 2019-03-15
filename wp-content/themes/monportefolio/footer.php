75f5bbc7dc3fd3d070f4cfe87f16bae958fd3b01
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-15 14:40:19 -0400

    Factorisation header - footer - sidebar

---
+++ wp-content/themes/monportefolio/footer.php
@@ -0,0 +1,50 @@
+	<!-- subscribe -->
+	<div class="subscribe">
+		<div class="grid-row clear">
+			<div class="them-mask"></div>
+			<div class="subscribe-header">subscribe</div>
+			<form action="#" class="clear">
+				<input type="email" placeholder="Your Email Address">
+				<button type="submit" class="button-send">Send</button>
+			</form>
+		</div>
+	</div>
+	<!--/ subscribe  -->
+	<!-- page footer  -->
+	<footer id="footer">
+		<div class="grid-row clear">
+			<div class="footer">
+				<div id="copyright">Copyright<span> Splashes</span> 2016-All Rights Reserved</div>
+				<a href="index.html" class="footer-logo"><img src="images/logo.png" alt=""></a>
+				<div class="social">
+					<a href=""><div class="contact-round"><i class="fa fa-twitter"></i></div></a>
+					<a href=""><div class="contact-round"><i class="fa fa-facebook"></i></div></a>
+					<a href=""><div class="contact-round"><i class="fa fa-skype"></i></div></a>
+					<a href=""><div class="contact-round"><i class="fa fa-rss"></i></div></a>
+					<a href=""><div class="contact-round"><i class="fa fa-linkedin"></i></div></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--/ page footer  -->
+	<a href="#" id="scroll-top" class="scroll-top"><i class="fa fa-angle-up"></i></a>
+	
+	<!-- scripts -->
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox_packed.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.fancybox-media.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.owl.carousel.js"></script>
+	
+	<script src="js/main.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/retina.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/jquery.tweet.js"></script>
+	<script src="js/jflickrfeed.min.js"></script>
+	<!--/ scripts -->
+
+</body>
+</html>
---
+++ wp-content/themes/monportefolio/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Blog Full Width</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<!--styles -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/jquery.fancybox.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/jquery.owl.carousel.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css">
+	<!--styles -->
+</head>
+<body class="blog with-sidebar pc">
+	<!-- page header -->
+	<header id="home">
+		<div class="stick-wrapper">
+			<div class="sticky clear">
+				<div class="grid-row">
+					<img class="splash" src="img/splash.png" alt="">
+					<div class="logo">
+						<a href="index.html"><img src="images/logo.png" alt=""></a>
+					</div>
+					<nav class="nav">
+						<a href="#" class="switcher">
+							<i class="fa fa-bars"></i>
+						</a>
+						<ul class="clear">
+							<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Accueil</a></li>
+							<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">&Agrave; propos de moi</a></li>
+							<li class="last"><a href="<?php echo esc_url( home_url( '/' ) ); ?>contact/">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!--/ page header  -->
