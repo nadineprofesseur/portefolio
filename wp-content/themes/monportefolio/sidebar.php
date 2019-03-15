fe6a473018128d7934b5d47d7727c7611485bbcb
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-15 15:00:56 -0400

    sidebar simplifiee

--- wp-content/themes/monportefolio/sidebar.php
+++ wp-content/themes/monportefolio/sidebar.php
@@ -41,168 +41,8 @@
 					</ul>
 				</div>
 				<!-- categories widget -->
-				<!--hr class="divider-green"-->
-				<!-- recent comments widget -->
-				<!--div class="widget widget-recent-comments clear">
-					<div class="widget-title">Our Comments</div>
-					<div class="carousel-nav">
-						<div class="carousel-button">
-							<div class="prev"><i class="fa fa-chevron-left"></i></div>
-							<div class="next"><i class="fa fa-chevron-right"></i></div>
-						</div>
-					</div>
-					<div id="recent-comments" class="owl-carousel owl-widget">
-							<ul>
-								<li><p>21.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-								<li><p>19.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-								<li><p>16.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-							</ul>
-						
-							<ul>
-								<li><p>09.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-								<li><p>02.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-								<li><p>29.12.2015</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-							</ul>
-						
-							<ul>
-								<li><p>20.12.2015</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-								<li><p>15.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-								<li><p>11.01.2016</p><p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p></li>
-							</ul>
-						
-					</div>
-				</div-->
-				<!-- recent comments widget -->
 				<hr class="divider-green">
-				<!-- text widget -->
-				<!--div id="text" class="widget widget_text">
-					<div class="widget-title">Text Widget</div>
-					<div class="textwidget">
-						<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor. Praesent dapibus aliquam erat, nec faucibus quam posuere eget. Vestibulum a accumsan.<br/><br/>
-Vestibulum a libero eu eros pulvinar congue dictum in nunc. Duis eget velit quis elit volutpat consequat a sed sem. Mauris sollicitudin lacinia eros et.</p>
-					</div>
-				</div-->
-				<!-- text widget -->
-				<!--hr class="divider-green"-->
-				<!-- recent post widget -->
-				<!--div class="widget widget-recent-posts">
-					<div class="widget-title">Recent Posts</div>
-					<div class="carousel-nav">
-						<div class="carousel-button">
-							<div class="prev"><i class="fa fa-chevron-left"></i></div>
-							<div class="next"><i class="fa fa-chevron-right"></i></div>
-						</div>
-					</div>
-					<div id="recent-posts-carousel" class="owl-carousel owl-widget">
-						<div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-1.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>29.12.2015</p>
-							</div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-2.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>21.01.2016</p>
-							</div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-3.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>12.01.2016</p>
-							</div>
-						</div>
-						<div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-4.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>01.01.2016</p>
-							</div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-5.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>11.11.2015</p>
-							</div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-6.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>01.11.2015</p>
-							</div>
-						</div>
-						<div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-7.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>31.10.2015</p>
-							</div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-8.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>12.10.2015</p>
-							</div>
-							<div class="recent-post-item clear">
-								<div class="recent-post-icon">
-									<div class="border-img">
-										<a href="blog-single-post.html"><div class="window-tabs">
-											<div class="overflow-block"></div><img src="images/recent-posts/recent-post-9.jpg" alt></div>
-										</a>
-									</div>
-								</div>
-								<p>Maecenas sit amet tortor ipsum. Sed ut semper dolor.</p>
-								<p>08.10.2015</p>
-							</div>
-						</div>
-					</div>
-				</div-->
-				<!-- recent post widget -->
-				<!--hr class="divider-green"-->
+
 				<!-- archives widget -->
 				<div id="archives" class="widget widget_archive">
 					<div class="widget-title">Archives</div>
@@ -218,14 +58,6 @@ Vestibulum a libero eu eros pulvinar congue dictum in nunc. Duis eget velit quis
 				</div>
 				<!-- archives widget -->
 				<hr class="divider-green">
-				<!-- widget calendar -->
-				<!--div class="widget widget-calendar">
-					<div class="widget-title">Calendar</div>
-					<div id="calendar"></div>
-				</div-->
-				<!--/ widget calendar -->
-				<!--hr class="divider-green"-->
-				<!-- meta widget -->
 				<div id="meta" class="widget widget_meta">
 					<div class="widget-title">Meta</div>
 					<ul>
@@ -236,102 +68,14 @@ Vestibulum a libero eu eros pulvinar congue dictum in nunc. Duis eget velit quis
 				</div>
 				<!-- meta widget -->
 				<hr class="divider-green">
-				<!-- widget twitter -->
-				<!--div class="widget widget-twitter">
-					<div class="widget-title">Latest Twitters</div>
-					<ul class="latest-tweets latest-tweets-alt"></ul>
-				</div-->
-				<!--/ widget twitter -->
-				<!--hr class="divider-green"-->
-				<!-- tags widget -->
 				<div id="tag_cloud-2" class="widget widget_tag_cloud">
 					<div class="widget-title">Tags</div>
 					<div class="tagcloud">
 						<ul>
 							<li><?php wp_tag_cloud( 'separator=</li><li>' ); ?></li>
 						</ul>
-						<!--ul>
-							<li><a href="#">Daily</a></li>
-							<li><a href="#">Design</a></li>
-							<li><a href="#">Ilustration</a></li>
-							<li><a href="#">Journal</a></li>
-							<li><a href="#">Label</a></li>
-							<li><a href="#">Photo</a></li>
-							<li><a href="#">Professional</a></li>
-							<li><a href="#">Show</a></li>
-							<li><a href="#">Sound</a></li>
-							<li><a href="#">Sounds</a></li>
-							<li><a href="#">Tv</a></li>
-							<li><a href="#">Video</a></li>
-						</ul-->
 					</div>
 				</div>
-				<!-- tags widget -->
-				<!--hr class="divider-green"-->
-				<!-- gallery widget -->
-				<!--div class="widget widget-gallery">
-					<div class="widget-title">Mini Gallery</div>
-					<div class="carousel-nav">
-						<div class="carousel-button">
-							<div class="prev"><i class="fa fa-chevron-left"></i></div>
-							<div class="next"><i class="fa fa-chevron-right"></i></div>
-						</div>
-					</div>
-					<div id="carousel-gallery-widget" class="owl-carousel owl-widget">
-						<div class="item-gallery-image picture">
-							<div class="hover-effect"></div>
-							<div class="link-cont">
-								<a href="#" class="cws-slide-left fancy fa fa-link"></a>
-								<a href="images/portfolio_three/four-columns-5@2x.jpg" class="fancy fa fa-search"></a>
-								<a href="#" class="cws-slide-right fa fa-heart"></a>
-							</div>
-							<img src="images/portfolio_three/four-columns-5.jpg" alt="">
-						</div>
-						<div class="item-gallery-image picture">
-							<div class="hover-effect"></div>
-							<div class="link-cont">
-								<a href="#" class="cws-slide-left fancy fa fa-link"></a>
-								<a href="images/portfolio_three/four-columns-6@2x.jpg" class="fancy fa fa-search"></a>
-								<a href="#" class="cws-slide-right fa fa-heart"></a>
-							</div>
-							<img src="images/portfolio_three/four-columns-6.jpg" alt="">
-						</div>
-						<div class="item-gallery-image picture">
-							<div class="hover-effect"></div>
-							<div class="link-cont">
-								<a href="#" class="cws-slide-left fancy fa fa-link"></a>
-								<a href="images/portfolio_three/four-columns-8@2x.jpg" class="fancy fa fa-search"></a>
-								<a href="#" class="cws-slide-right fa fa-heart"></a>
-							</div>
-							<img src="images/portfolio_three/four-columns-8.jpg" alt="">
-						</div>
-						<div class="item-gallery-image picture">
-							<div class="hover-effect"></div>
-							<div class="link-cont">
-								<a href="#" class="cws-slide-left fancy fa fa-link"></a>
-								<a href="images/portfolio_three/four-columns-9@2x.jpg" class="fancy fa fa-search"></a>
-								<a href="#" class="cws-slide-right fa fa-heart"></a>
-							</div>
-							<img src="images/portfolio_three/four-columns-9.jpg" alt="">
-						</div>
-						<div class="item-gallery-image picture">
-							<div class="hover-effect"></div>
-							<div class="link-cont">
-								<a href="#" class="cws-slide-left fancy fa fa-link"></a>
-								<a href="images/portfolio_three/four-columns-10@2x.jpg" class="fancy fa fa-search"></a>
-								<a href="#" class="cws-slide-right fa fa-heart"></a>
-							</div>
-							<img src="images/portfolio_three/four-columns-10.jpg" alt="">
-						</div>
-					</div>
-				</div-->
-				<!-- gallery widget -->
-				<!--hr class="divider-green"-->
-				<!-- flickr widget -->
-				<!--div class="widget widget_flickr">
-					<div class="widget-title">Flickr</div>
-					<ul id="flickr-badge" class="clear"></ul>
-				</div-->
 				<!-- flickr widget -->
 				<hr class="divider-green">
 			</aside>
